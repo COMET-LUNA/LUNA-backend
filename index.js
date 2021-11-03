2a4e7e4392c800f99f70abc35d9d6c821453a2e2
seanpe11 <sean.m.s.pe@gmail.com>
2021-11-03 18:33:51 +0800

    added firebase

---
+++ index.js
@@ -0,0 +1,13 @@
+import { initalizeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMittekZsL95o57e6kyERNtnd7pwZL8fk",
+  authDomain: "luna-doctor-finder.firebaseapp.com",
+  projectId: "luna-doctor-finder",
+  storageBucket: "luna-doctor-finder.appspot.com",
+  messagingSenderId: "642725602228",
+  appId: "1:642725602228:web:a7847cb3670f4e700248e1",
+  measurementId: "G-ZXDTFTXV85"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
