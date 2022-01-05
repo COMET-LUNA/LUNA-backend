556049d1bc0081b2e75e1eadd0a69fce67d9416c
seanpe11 <sean.m.s.pe@gmail.com>
2022-01-05 15:43:22 +0800

    slight directory structure

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-import { initalizeApp } from 'firebase/app';
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3030
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCMittekZsL95o57e6kyERNtnd7pwZL8fk",
-  authDomain: "luna-doctor-finder.firebaseapp.com",
-  projectId: "luna-doctor-finder",
-  storageBucket: "luna-doctor-finder.appspot.com",
-  messagingSenderId: "642725602228",
-  appId: "1:642725602228:web:a7847cb3670f4e700248e1",
-  measurementId: "G-ZXDTFTXV85"
-};
+var findMeRouter = require('./routes/findMe')
 
-const firebaseApp = initializeApp(firebaseConfig);
+app.use("/findMe", findMeRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
