ba7e96be85fb0998a7215f27a3293d2eaf794506
jolocansana <jcansana@gmail.com>
2022-01-19 15:30:00 +0800

    send user info on login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 // const users = require('../data/users.json')
 const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} = require("firebase/auth")
+const {getFirestore, collection, query, where, getDocs} = require('firebase/firestore')
 
 var express = require('express')
 var router = express.Router()
 router.use(express.json())
+
+const firestore = getFirestore();
+const usersDb = collection(firestore, 'users')
 
 router.post('/login', function(req, res){
   console.log(req.body)
@@ -16,7 +20,16 @@ router.post('/login', function(req, res){
   // verify logic
   signInWithEmailAndPassword(auth, email, password).then(user => {
     status = "ok"
-    res.send({status})
+    console.log(user.user.email)
+    const q = query(usersDb, where("email", "==", user.user.email))
+    getDocs(q).then(data => {
+      var userData = {}
+      data.forEach(doc => {
+        // console.log(doc.data())
+        userData = doc.data()
+      })
+      res.send({userData, status})
+    })
   }).catch((error) => {
     console.log(error.code, error.message)
     res.send({status})
